4a7aa806a8cd8bdd760e64b6ef1f0492ba9295e6
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-01-10 12:13:18 -0500

    Output dynamic spectrum and "fit" it.
    Using a new theta-theta plot constructor.

--- screen2ds.py
+++ screen2ds.py
@@ -6,7 +6,10 @@ Create a dynamic wave field based on a set of scattering points.
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy import units as u, constants as const
+from astropy.time import Time
 from astropy.visualization import quantity_support
+
+from scintillometry.io import hdf5
 
 from fields import dynamic_field
 
@@ -22,25 +25,41 @@ fobs = const.c / lobs
 d_eff = 1 * u.kpc
 mu_eff = 100 * u.mas / u.yr
 
+simple = True
+
 # Create scattering screen, composed of a centred and an offset Gaussian.
-th1 = np.linspace(-8, 8, 64, endpoint=False) << u.mas
-th2 = np.array([4.5, 4.7, 4.8, 4.9, 5., 5.2, 5.5, 6.5, 8.5]) << u.mas
 sig = 1.5*u.mas
-a1 = 0.3*np.exp(-0.5*(th1/sig)**2)
-a2 = 0.03 * np.exp(-0.5*((th2-6*u.mas)/sig)**2)
-th = np.hstack((th1.value, th2.value)) << u.mas
-a = np.hstack((a1.value, a2.value))
-th_perp = np.hstack((-th1.value/20, .2*np.ones(th2.shape))) << u.mas
+if simple:
+    th = np.linspace(-8, 8, 32, endpoint=False) << u.mas
+    th_perp = np.zeros_like(th)
+    a = 0.3*np.exp(-0.5*(th/sig)**2) + 0.03*np.exp(-0.5*((th-6*u.mas)/sig)**2)
+else:
+    th1 = np.linspace(-8, 8, 32, endpoint=False) << u.mas
+    a1 = 0.3*np.exp(-0.5*(th1/sig)**2)
+    th2 = np.array([4.5, 4.7, 4.8, 4.9, 5., 5.2, 5.5, 6.5, 8.5]) << u.mas
+    a2 = 0.03 * np.exp(-0.5*((th2-6*u.mas)/sig)**2)
+    th = np.hstack((th1.value, th2.value)) << u.mas
+    a = np.hstack((a1.value, a2.value))
+    th_perp = np.hstack((-th1.value/20, .2*np.ones(th2.shape))) << u.mas
+
 realization = a * np.random.normal(size=th.shape+(2,)).view('c16').squeeze(-1)
 # Make direct line of sight a bit brighter.
-realization[th1.size // 2] = 1
+# TODO: this image should not really have proper motion!
+realization[np.where(th == 0)] = 1
 # realization[th.size // 2-4] = 0.1
+
+# Normalize so we should get a dynamic spectrum of unity mean
+realization /= (np.abs(realization)**2).sum()
 
 # plt.plot(th, realization.real)
 
+# Smallest theta corresponds to 0.15 us -> 6.5 MHz of bandwidth
+# But also need resolution of 0.013 MHz -> factor 500.
+# (d_eff/const.c/2*(0.25*u.mas)**2).to(u.us, u.dimensionless_angles())
+
 f = np.linspace(-0.5, 0.5, 200, endpoint=False) << u.MHz
 f += fobs
-t = (np.linspace(-500, 500, 80, endpoint=False) << u.s).to(u.minute)
+t = (np.linspace(-240, 240, 40, endpoint=False) << u.s).to(u.minute)
 
 
 ax1 = plt.subplot(131)
@@ -52,7 +71,7 @@ ax1.set_aspect(1.)
 dynwave = dynamic_field(th, th_perp, realization, d_eff, mu_eff, f, t)
 
 # Just for fun, add noise.
-noise = np.random.normal(scale=0.05,
+noise = np.random.normal(scale=0.01,
                          size=dynwave.shape[-2:]+(2,)).view('c16').squeeze(-1)
 axes = tuple(range(0, dynwave.ndim-2))
 dynspec = np.abs(dynwave[...].sum(axes) + noise)**2
@@ -85,3 +104,13 @@ plt.imshow(np.log10(ss), origin=0, aspect='auto', extent=sec_extent,
 plt.xlabel(fd.unit.to_string('latex'))
 plt.ylabel(tau.unit.to_string('latex'))
 plt.colorbar()
+
+
+with hdf5.open('dynspec.h5', 'w', sample_shape=dynspec.shape[:1],
+               sample_rate=(1/(t[1]-t[0])).to(u.mHz),
+               samples_per_frame=dynspec.shape[-1],
+               dtype=dynspec.dtype, time=Time.now(), frequency=f,
+               sideband=1) as fw:
+    fw.write(dynspec.T)
+    fw.fh_raw.create_dataset('realization', data=realization.value)
+    fw.fh_raw.create_dataset('theta', data=th.value)
---
+++ theta.py
@@ -0,0 +1,109 @@
+import numpy as np
+from astropy import units as u
+from astropy.visualization import quantity_support
+from matplotlib import pyplot as plt
+
+from scintillometry.io import hdf5
+
+from fields import dynamic_field
+
+
+plt.ion()
+quantity_support()
+plt.clf()
+
+
+with hdf5.open('dynspec.h5') as fh:
+    dynspec = fh.read().T
+    f = fh.frequency
+    fobs = f[f.shape[0]//2]
+    t = (np.arange(-fh.shape[0] // 2, fh.shape[0] // 2)
+         / fh.sample_rate).to(u.minute)
+    realization = fh.fh_raw['realization'][:]
+    th = fh.fh_raw['theta'][:] << u.mas
+
+# Display simulated dynamic spectrum.
+ds_kwargs = dict(extent=(t[0].value, t[-1].value, f[0].value, f[-1].value),
+                 origin=0, aspect='auto', cmap='Greys', vmin=0, vmax=6)
+plt.subplot(321)
+plt.imshow(dynspec, **ds_kwargs)
+plt.xlabel(t.unit.to_string('latex'))
+plt.ylabel(f.unit.to_string('latex'))
+plt.colorbar()
+
+d_eff = 1 * u.kpc
+mu_eff = 100 * u.mas / u.yr
+
+dynwave = dynamic_field(th, 0, 1., d_eff, mu_eff, f, t)
+
+# Get intensities by brute-force mapping.
+pairs = dynwave * dynwave[:, np.newaxis].conj()
+# Remove constant parts
+pairs -= pairs.mean((-2, -1), keepdims=True)
+th_th = (dynspec * pairs).mean((-2, -1))
+
+# Clean up near the diagonal
+k = 1
+th_th = np.triu(th_th, k=k) + np.tril(th_th, k=-k)
+# th_th[27:38, 27:38] = 0
+i = np.arange(th.size-1)
+th_th[th.size-1-i, i+1] = 0
+
+# Show inferred theta-theta.
+th_kwargs = dict(extent=(th[0].value, th[-1].value)*2,
+                 origin=0, vmin=-7, vmax=0, cmap='Greys')
+plt.subplot(322)
+plt.imshow(np.log10(np.maximum(np.abs(th_th)**2, 1e-30)), **th_kwargs)
+plt.xlabel(th.unit.to_string('latex'))
+plt.ylabel(th.unit.to_string('latex'))
+
+
+# Calculate eigenvectors for inferred theta-theta.
+
+w, v = np.linalg.eigh(th_th)
+
+assert w[-1] == w.max()
+
+recovered = v[:, -1] * w[-1]
+
+# Show the theta-theta implied by largest eigenvector.
+plt.subplot(324)
+plt.imshow(np.log10(np.abs(np.outer(recovered, recovered.conj())**2)),
+           **th_kwargs)
+plt.xlabel(th.unit.to_string('latex'))
+plt.ylabel(th.unit.to_string('latex'))
+
+# As well as the corresponding dynamic spetrum.
+plt.subplot(323)
+dynwave_r = dynamic_field(th, 0, recovered, d_eff, mu_eff, f, t)
+dynspec_r = np.maximum(np.abs(dynwave_r.sum(0)) ** 2, 1e-30)
+# Mean of dynamic spectra should equal sum of all recovered powers.
+# Since we normalize that to 1, do the same here
+dynspec_r /= dynspec_r.mean()
+plt.imshow(dynspec_r, **ds_kwargs)
+plt.xlabel(t.unit.to_string('latex'))
+plt.ylabel(f.unit.to_string('latex'))
+plt.colorbar()
+
+# Also show theta-theta corresponding to actual realization.
+plt.subplot(326)
+th_th_real = np.outer(realization, realization.conj())
+plt.imshow(np.log10(np.abs(th_th_real**2)), **th_kwargs)
+plt.xlabel(th.unit.to_string('latex'))
+plt.ylabel(th.unit.to_string('latex'))
+
+w_real, v_real = np.linalg.eigh(th_th_real)
+
+assert w_real[-1] == w_real.max()
+
+recovered_real = v_real[:, -1] * w_real[-1]
+
+# and the dynamic spectrum implied by its largest eigenvector.
+plt.subplot(325)
+dynwave_real = dynamic_field(th, 0, recovered_real, d_eff, mu_eff, f, t)
+dynspec_real = np.maximum(np.abs(dynwave_real.sum(0)) ** 2, 1e-30)
+dynspec_real /= (np.abs(recovered_real)**2).sum()
+plt.imshow(dynspec_real, **ds_kwargs)
+plt.xlabel(t.unit.to_string('latex'))
+plt.ylabel(f.unit.to_string('latex'))
+plt.colorbar()
