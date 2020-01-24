c3080563d47e1863b54783b375514437eddb00be
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-01-24 16:26:13 -0500

    Split off script parts

--- dynspec.py
+++ dynspec.py
@@ -1,8 +1,6 @@
 import numpy as np
 from astropy import units as u, constants as const
 from astropy.table import QTable
-from astropy.visualization import quantity_support
-from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.linalg import eigh
 import h5py
@@ -10,7 +8,6 @@ import h5py
 from scintillometry.io import hdf5
 
 from fields import dynamic_field, theta_theta, theta_grid
-from visualization import ThetaTheta
 
 
 class DynamicSpectrum:
@@ -361,85 +358,3 @@ class DynamicSpectrum:
 
         return (self.cln_mag_fit, self.cln_mag_err,
                 self.cln_mu_eff_fit, self.cln_mu_eff_err)
-
-
-if __name__ == '__main__':
-    plt.ion()
-    quantity_support()
-
-    dyn_chi2 = DynamicSpectrum.fromfile('dynspec.h5', d_eff=1.*u.kpc,
-                                        mu_eff=100*u.mas/u.yr)
-    dyn_chi2.theta = dyn_chi2.theta_grid(
-        tau_max=(1./(dyn_chi2.f[3]-dyn_chi2.f[0])).to(u.us))
-    # if not hasattr(dyn_chi2, 'curvature'):
-    dyn_chi2.locate_mu_eff(np.arange(98, 103) << u.mas/u.yr)
-
-    r = dyn_chi2.curvature
-
-    plt.clf()
-    plt.subplot(3, 4, 1)
-    plt.plot(r['mu_eff'], r['redchi2'])
-
-    th_th = dyn_chi2.theta_theta(r.meta['mu_eff'])
-    th = dyn_chi2.theta
-    th_kwargs = dict(extent=(th[0].value, th[-1].value)*2,
-                     origin=0, vmin=-7, vmax=0, cmap='Greys')
-    th_th_proj = ThetaTheta(dyn_chi2.theta)
-    plt.subplot(3, 4, 2, projection=th_th_proj)
-    plt.imshow(np.log10(np.maximum(np.abs(th_th)**2, 1e-30)), **th_kwargs)
-    plt.xlabel(th.unit.to_string('latex'))
-    plt.ylabel(th.unit.to_string('latex'))
-
-    ds_kwargs = dict(extent=(dyn_chi2.t[0].value, dyn_chi2.t[-1].value,
-                             dyn_chi2.f[0].value, dyn_chi2.f[-1].value),
-                     origin=0, aspect='auto', cmap='Greys',
-                     vmin=0, vmax=7)
-    ibest = r['redchi2'].argmin()
-    dynspec_r = dyn_chi2.model(r['recovered'][ibest],
-                               mu_eff=r['mu_eff'][ibest])
-    dynspec_r *= dyn_chi2.dynspec.mean()/dynspec_r.mean()
-    plt.subplot(3, 4, 3)
-    plt.imshow(dynspec_r.T, **ds_kwargs)
-
-    res_kwargs = ds_kwargs.copy()
-    res_kwargs['vmin'], res_kwargs['vmax'] = -5, 5
-    plt.subplot(3, 4, 4)
-    plt.imshow((dyn_chi2.dynspec-dynspec_r).T/dyn_chi2.noise, **res_kwargs)
-
-    plt.subplot(3, 4, 7)
-    plt.imshow(dyn_chi2.dynspec.T, **ds_kwargs)
-
-    (raw_mag_fit, raw_mag_err,
-     raw_mu_eff_fit, raw_mu_eff_err) = dyn_chi2.fit(
-         guess='curvature', verbose=3,
-         ftol=0.1/dyn_chi2.dynspec.size)
-
-    plt.subplot(3, 4, 5)
-    plt.plot(th, np.abs(raw_mag_fit), th, np.abs(raw_mag_err))
-
-    rd = np.sqrt(dyn_chi2.pcovar.diagonal())
-    rc = dyn_chi2.pcovar/rd/rd[:, np.newaxis]
-    plt.subplot(3, 4, 6)
-    plt.imshow(rc, vmin=-1, vmax=1)
-
-    plt.subplot(3, 4, 8)
-    plt.imshow(dyn_chi2.residuals(raw_mag_fit, raw_mu_eff_fit).T,
-               **res_kwargs)
-
-    (cln_mag_fit, cln_mag_err,
-     cln_mu_eff_fit, cln_mu_eff_err) = dyn_chi2.cleanup_fit()
-
-    plt.subplot(3, 4, 9)
-    plt.plot(th, np.abs(cln_mag_fit), th, np.abs(cln_mag_err))
-
-    cd = np.sqrt(dyn_chi2.ccovar.diagonal())
-    cc = dyn_chi2.ccovar/cd/cd[:, np.newaxis]
-    plt.subplot(3, 4, 10)
-    plt.imshow(cc, vmin=-1, vmax=1)
-
-    plt.subplot(3, 4, 11)
-    plt.imshow(dyn_chi2.model(cln_mag_fit, cln_mu_eff_fit).T,
-               **ds_kwargs)
-    plt.subplot(3, 4, 12)
-    plt.imshow(dyn_chi2.residuals(cln_mag_fit, cln_mu_eff_fit).T,
-               **res_kwargs)
---
+++ try_sec_fit.py
@@ -0,0 +1,79 @@
+import numpy as np
+from astropy import units as u
+from matplotlib import pyplot as plt
+from astropy.visualization import quantity_support
+from scipy.linalg import eigh
+
+from screens.visualization import ThetaTheta
+from screens import DynamicSpectrum, SecondarySpectrum
+
+
+plt.ion()
+plt.clf()
+quantity_support()
+
+dyn_spec = DynamicSpectrum.fromfile('dynspec.h5', d_eff=1.*u.kpc,
+                                    mu_eff=100*u.mas/u.yr)
+sec_spec = SecondarySpectrum.from_dynamic_spectrum(dyn_spec)
+
+sec_kwargs = dict(extent=(sec_spec.fd[0].value, sec_spec.fd[-1].value,
+                          sec_spec.tau[0].value, sec_spec.tau[-1].value),
+                  cmap='Greys', vmin=-7, vmax=0, origin=0, aspect='auto')
+plt.subplot(321)
+plt.imshow(np.log10(np.abs(sec_spec.secspec)**2).T, **sec_kwargs)
+
+conserve = True
+
+mu_eff = 100*u.mas/u.yr
+th_kwargs = sec_kwargs.copy()
+th_kwargs['extent'] = (sec_spec.theta[0].value,
+                       sec_spec.theta[-1].value)*2
+th_kwargs['aspect'] = 'equal'
+th_th = sec_spec.theta_theta(mu_eff, conserve=conserve)
+th_th_proj = ThetaTheta(sec_spec.theta)
+ax = plt.subplot(322, projection=th_th_proj)
+ax.imshow(np.log10(np.maximum(np.abs(th_th)**2, 1e-30)).T, **th_kwargs)
+
+secspec = sec_spec.secspec.copy()
+secspec[(sec_spec.fd == 0) | (sec_spec.tau == 0)] = 0
+# try recoving just plain power
+w_a, v_a = eigh(np.abs(th_th)**2, eigvals=(sec_spec.theta.size-1,)*2)
+rec_a = v_a[:, -1] * np.sqrt(w_a[-1])
+th_th_rp = rec_a[:, np.newaxis] * rec_a
+ax = plt.subplot(324, projection=th_th_proj)
+ax.imshow(np.log10(np.maximum(th_th_rp, 1e-30)).T, **th_kwargs)
+
+sec_rp = sec_spec.model(rec_a, mu_eff, conserve=conserve)
+sec_p = np.abs(secspec)**2
+sec_p_noise = sec_p[:30, :30].std()
+print("just power, red chi2 = ",
+      ((np.abs(secspec)**2-sec_rp)**2).mean() / sec_p_noise**2)
+plt.subplot(323)
+plt.imshow(np.log10(np.maximum(sec_rp, 1e-30)).T, **sec_kwargs)
+
+# try recovering phases as well.
+w, v = eigh(th_th, eigvals=(sec_spec.theta.size-1,)*2)
+recovered = v[:, -1] * np.sqrt(w[-1])
+th_th_r = recovered[:, np.newaxis] * recovered
+ax = plt.subplot(326, projection=th_th_proj)
+ax.imshow(np.log10(np.maximum(np.abs(th_th_r)**2, 1e-30)).T, **th_kwargs)
+
+sec_r = sec_spec.model(recovered, mu_eff, conserve=conserve)
+sec_noise = secspec[:30, :30].std()
+print("also phase, red chi2 = ",
+      (np.abs(secspec-sec_r)**2).mean() / sec_noise**2)
+print(" power red chi2 = ",
+      ((np.abs(secspec)**2-np.abs(sec_r)**2)**2).mean() / sec_p_noise**2)
+plt.subplot(325)
+plt.imshow(np.log10(np.maximum(np.abs(sec_r)**2, 1e-30)).T, **sec_kwargs)
+
+# Try via dynspec.  Generically, this seems to be worse, especially with
+# flux conservation turned on.
+dyn_spec.theta = sec_spec.theta
+ds_model = dyn_spec.model(recovered)
+sec_dr = np.fft.fft2(ds_model)
+sec_dr /= sec_dr[0, 0]
+sec_dr = np.fft.fftshift(sec_dr)
+sec_dr[(sec_spec.fd == 0) | (sec_spec.tau == 0)] = 0
+print("via dyn spec, red chi2 = ",
+      (np.abs(secspec-sec_dr)**2).mean() / sec_noise**2)
