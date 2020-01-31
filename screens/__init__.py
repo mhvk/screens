c266a2f478d2cc7dc8d1e909ab41cbad0b2c4b41
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-01-31 17:22:16 -0500

    Adjust __init__.py back to package template.

--- screens/__init__.py
+++ screens/__init__.py
@@ -1,3 +1,17 @@
 # Licensed under the GPLv3 - see LICENSE
+# Licensed under the GPLv3 - see LICENSE
+"""Pulsar Scintillation Screen modelling."""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *   # noqa
+# ----------------------------------------------------------------------------
+
+if not _ASTROPY_SETUP_:   # noqa
+    # For egg_info test builds to pass, put package imports here.
+    pass
+
+
 from .dynspec import DynamicSpectrum
 from .secspec import SecondarySpectrum
