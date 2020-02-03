65a09c91671837cdc31031271c6d77da99d31539
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-02-03 12:16:20 -0500

    Let __init__ only import if not in setup.

--- screens/__init__.py
+++ screens/__init__.py
@@ -1,5 +1,4 @@
 # Licensed under the GPLv3 - see LICENSE
-# Licensed under the GPLv3 - see LICENSE
 """Pulsar Scintillation Screen modelling."""
 
 # Affiliated packages may add whatever they like to this file, but
@@ -10,8 +9,5 @@ from ._astropy_init import *   # noqa
 
 if not _ASTROPY_SETUP_:   # noqa
     # For egg_info test builds to pass, put package imports here.
-    pass
-
-
-from .dynspec import DynamicSpectrum
-from .secspec import SecondarySpectrum
+    from .dynspec import DynamicSpectrum  # noqa
+    from .secspec import SecondarySpectrum  # noqa
